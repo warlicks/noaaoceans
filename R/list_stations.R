746a8fddd173d8b3cbac91c6cbd02805ccbae288
warlicks <warlick.sean@gmail.com>
2018-08-25 01:47:27 -0700

    Inital development of a function to retrive data from NOAA ocean station list.

---
+++ R/list_stations.R
@@ -0,0 +1,57 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+list_stations <- function(){
+    # Call the URL with station data.
+    station_url <- 'https://opendap.co-ops.nos.noaa.gov/stations/stationsXML.jsp'
+    station_xml <- xml2::read_html(station_url)
+
+    # Get Station Names
+    station_nodes <- rvest::html_nodes(station_xml, 'station')
+    station_names <- rvest::html_attr(station_nodes, 'name')
+    station_id <- rvest::html_attr(station_nodes, 'id')
+
+    # Parse out the stations location.  We have state, lat and long data to help
+    # locate a given station.
+    station_lat_nodes <- rvest::html_nodes(station_xml, 'lat')
+    station_lat <- rvest::html_text(station_lat_nodes)
+
+    station_long_nodes <- rvest::html_nodes(station_xml, 'long')
+    station_long <- rvest::html_text(station_long_nodes)
+
+    station_state_nodes <- rvest::html_nodes(station_xml, 'state')
+    station_state <- rvest::html_text(station_state_nodes)
+
+    # Parse the data a station was founded.
+    date_established_nodes <- rvest::html_nodes(station_xml, 'date_established')
+    date_established <- rvest::html_text(date_established_nodes)
+
+
+    # Create a data frame with the station info.
+    station_df <- data.frame(station_id,
+                             station_names,
+                             station_state,
+                             station_lat,
+                             station_long,
+                             date_established)
+
+    # We want to add columns for all the possible sensor names. We start by
+    # finding all the parameter nodes.
+    parameter_nodes <- rvest::html_nodes(station_xml, 'parameter')
+
+    # We then extract all the sensor names and create list of unique names
+    sensor_names <- rvest::html_attr(parameter_nodes, 'name')
+    sensor_names <- unique(sensor_names)
+    sensor_names <- sensor_names[sensor_names != ""]
+
+    # We now add the unique sensor names as columns to our data frame.
+    station_df[, sensor_names] <- NA
+
+    status_station_df <- parse_sensor_status(station_nodes, station_df)
+
+    return(status_station_df)
+}
---
+++ R/parse_sensor_status.R
@@ -0,0 +1,39 @@
+#' Title
+#'
+#' @param station_nodes
+#' @param data_frame
+#'
+#' @return
+#' @noRd
+#'
+#' @examples
+parse_sensor_status <- function(station_nodes, data_frame){
+
+    for(i in 1:length(station_nodes)){
+        node_children <- rvest::html_nodes(station_nodes[i], 'parameter')
+        current_station_id <- rvest::html_attr(station_nodes[i], 'id')
+        station_sensors <- rvest::html_attrs(node_children)
+
+        if(length(station_sensors) < 1){
+            next()
+        }
+
+        for(j in 1:length(station_sensors)){
+            sensor_name <- station_sensors[[j]]['name']
+            sensor_status <- station_sensors[[j]]['status']
+
+            print(sensor_name)
+            print(sensor_status)
+
+            # Skip sensors without a name
+            if(sensor_name == ""){
+                next()
+            }
+
+             data_frame[data_frame$station_id == current_station_id,
+                        sensor_name] <- sensor_status
+        }
+    }
+
+    return(data_frame)
+}
