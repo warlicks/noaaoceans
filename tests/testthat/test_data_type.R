12f75102ebe99ac78597f7c3fd6adf2d2ecc6540
warlicks <warlick.sean@gmail.com>
2019-01-27 21:07:17 -0800

    Skip tests on CRAN

--- tests/testthat/test_data_type.R
+++ tests/testthat/test_data_type.R
@@ -2,6 +2,7 @@
 context('Test Returned Data Types')
 
 test_that('Expect that predictions data prodcut query returns a data frame', {
+    skip_on_cran()
     expect_is(query_coops_data('9414290',
                                '20170101',
                                '20170201',
@@ -13,6 +14,7 @@ test_that('Expect that predictions data prodcut query returns a data frame', {
 })
 
 test_that("Test that the function returns a data frame for non-prediction data products", {
+    skip_on_cran()
     expect_is(query_coops_data('9444900',
                                '20180601 15:00',
                                '20180601 17:00',
@@ -22,6 +24,7 @@ test_that("Test that the function returns a data frame for non-prediction data p
 })
 
 test_that('Test that the function can properly parse an error message', {
+    skip_on_cran()
     expect_error(query_coops_data('9414290',
                                   '20170101',
                                   '20170101',
@@ -30,5 +33,6 @@ test_that('Test that the function can properly parse an error message', {
 })
 
 test_that('Test that station list returns a data frame', {
+    skip_on_cran()
     expect_is(list_coops_stations(), 'data.frame')
 })
