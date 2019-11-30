b7eb5b12f705919877590a8b0da15081b77cade4
warlicks <warlick.sean@gmail.com>
2019-11-30 11:23:29 -0800

    Documented internal function to get and parse data inventory data.

--- R/process_inventory_table.R
+++ R/process_inventory_table.R
@@ -1,7 +1,25 @@
+#' Query Data Inventory Page and Process HTML
+#'
+#' An internal function used by \code{\link{coops_station_inventory}}. It gets
+#' the HTML for coops station and parses HTML to find the table with data
+#' inventory data. The table is return  as a data frame.
+#'
+#' @param query_url a character string provideing a URL for the data inventory
+#' page of a NOOA CO-OPS station.
+#'
+#' @return data frame
+#'
+#' @keywords internal
+#' @noRd
+#'
+#'
 process_inventory_table <- function(query_url){
-    # Call URL & process the returned HTML
+
+    # Call URL & process the returned HTML ----
     returned_html <- httr::GET(query_url)
     processed_html <- xml2::read_html(returned_html$content)
+
+    # Parse the HTML ----
 
     # Find the body of the html
     station_body <- rvest::html_node(processed_html, 'body')
